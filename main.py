9ebe70bca5b729d6ec590858132614f534a5a9ef
LcsMesquita <lucas.mequita1110@gmail.com>
2023-07-16 00:00:01 -0300

    First commit

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ url = "https://newsapi.org/v2/everything?q=tesla" \
     "&from=2023-06-16&sortBy=publishedAt&apiKey=" \
         "56f2115a07484add8b19537abb98a47c"
 
+# Make a request
 request = requests.get(url)
+
+# Get a dictionary with data
 content = request.json() #using information as a dictionary
 
+# Acess the article titles and description
 for article in content['articles']:
     print(article['title'])
